f2e5d0ec39b8a264ae1409e5aa9ba41f1b23a2d9
LilSmash <61291979+lilsmash720@users.noreply.github.com>
2025-10-23 18:40:07 -0500

    Delete test_audio_detect.py

--- test_audio_detect.py
+++
@@ -1,7 +0,0 @@
-from pycaw.pycaw import AudioUtilities
-
-print("🎧 Listing all audio sessions:")
-sessions = AudioUtilities.GetAllSessions()
-for session in sessions:
-    if session.Process:
-        print(f"- {session.Process.name()} | State: {session.State}")
